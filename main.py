ebb6954225fec5f074d029ded5ca0cdb78ab5659
Raghu <rrrreddy07@gmail.com>
2024-05-12 00:02:32 -0500

    logging added

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.cnnClassifier import logger
+
+logger.info("Welcome to my custom log")
+logger.debug("This is a debug message")
+logger.warning("This is a warning message")
+logger.error("This is an error message")
+logger.critical("This is a critical message")
+logger.fatal("This is a fatal message")
